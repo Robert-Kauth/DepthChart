653fce9078e33df8b2d8635162648d4893dee845
Robert Kauth <kauthr@gmail.com>
2021-11-16 13:41:52 -0800

    added flask avatar generator

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,11 +4,12 @@ from app.models import db, User
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
+        username='Demo', email='demo@aa.io', password='password',)
     marnie = User(
         username='marnie', email='marnie@aa.io', password='password')
     bobbie = User(
         username='bobbie', email='bobbie@aa.io', password='password')
+    ashley = User
 
     db.session.add(demo)
     db.session.add(marnie)
