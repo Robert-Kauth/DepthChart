4eafe46556f4abb92786b91f21a2c4f9fab53570
Robert Kauth <kauthr@gmail.com>
2021-11-16 07:45:40 -0800

    set up database and added .env file, started aws integration

--- app/config.py
+++ app/config.py
@@ -11,3 +11,9 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL').replace('postgres://', 'postgresql://')
     SQLALCHEMY_ECHO = True
+
+# TODO add S3 integration
+    # S3_BUCKET = os.environ.get("S3_BUCKET_NAME")
+    # S3_KEY = os.environ.get("S3_ACCESS_KEY")
+    # S3_SECRET = os.environ.get("S3_SECRET_ACCESS_KEY")
+    # S3_LOCATION = f"http://speak-easy.s3.amazonaws.com/"
