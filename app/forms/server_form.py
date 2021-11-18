917455f6cadc9ea16ce538d3c191b85fb945b911
Robert Kauth <kauthr@gmail.com>
2021-11-18 09:08:57 -0800

    removed server name unique function from server form

--- app/forms/server_form.py
+++ app/forms/server_form.py
@@ -5,16 +5,6 @@ from wtforms.validators import DataRequired, ValidationError
 from app.models import Server
 
 
-def name_exists(form, field):
-    '''
-    Checks if server name already exits in database
-    '''
-    name = field.data
-    server = Server.query.filter(Server.name == name).first()
-    if server:
-        raise ValidationError('Server name is already in use')
-
-
 class ServerForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
     topic = StringField('topic')
