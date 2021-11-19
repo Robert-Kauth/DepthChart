7093993ccd47ffe3c29b14959baa0f3d5bea22d9
Robert Kauth <kauthr@gmail.com>
2021-11-18 18:16:37 -0800

    newly created server now renders

--- react-app/src/components/Servers/Servers.js
+++ react-app/src/components/Servers/Servers.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import ServerTile from "./ServerTile";
 import CreateServerModal from "./CreateServerModal";
 import { getUserServers } from "../../store/user_servers";
+import { loadServers } from "../../store/servers";
 
 import styles from "./Servers.module.css";
 // className={styles. }
@@ -10,21 +11,29 @@ import styles from "./Servers.module.css";
 export default function Servers() {
     const dispatch = useDispatch();
 
-    const servers = useSelector((state) => Object.values(state.user_servers));
+    const servers = useSelector((state) => state.servers);
+    // const servers = useSelector((state) => Object.values(state.user_servers));
     const user_id = useSelector((state) => state.session.user.id);
 
     useEffect(() => {
         dispatch(getUserServers(user_id));
+        dispatch(loadServers());
     }, [dispatch, user_id]);
+
+    let user_servers;
+    if (servers) {
+        user_servers = Object.values(servers).map((server) => {
+            if (server.owner_id === user_id) {
+                return <ServerTile key={server.id} server={server} />;
+            }
+            return user_servers;
+        });
+    }
 
     return (
         <div className={styles.wrapper}>
-            <div className={styles.tileWrapper}>
-                {servers &&
-                    servers.map((server) => (
-                        <ServerTile key={server.id} server={server} />
-                    ))}
-            </div>
+            gs
+            <div className={styles.tileWrapper}>{user_servers}</div>
             <div className={styles.add}>
                 <CreateServerModal />
             </div>
