22d8a2f47729db27df70bca0fd5c0d63d8a19f15
Robert Kauth <kauthr@gmail.com>
2021-11-20 18:26:22 -0800

    styled signup form to look like login

--- react-app/src/components/Signup/SignupForm.js
+++ react-app/src/components/Signup/SignupForm.js
@@ -50,49 +50,56 @@ export default function SignupForm() {
             <form className={styles.form} onSubmit={onSignUp}>
                 <fieldset className={styles.field}>
                     <legend className={styles.legend}>Sign Up</legend>
-                    {errors.map((error, ind) => (
-                        <div>
-                            <legend>Errors:</legend>
+                    <div className={styles.errors}>
+                        {errors.map((error, ind) => (
                             <div key={ind}>{error}</div>
+                        ))}
+                    </div>
+                    <div className={styles.inputs}>
+                        <div className={styles.usernameWrapper}>
+                            <label className={styles.usernameLabel}>
+                                User Name:
+                            </label>
+                            <input
+                                type="text"
+                                name="username"
+                                onChange={updateUsername}
+                                value={username}></input>
                         </div>
-                    ))}
-                    <div>
-                        <label>User Name</label>
-                        <input
-                            type="text"
-                            name="username"
-                            onChange={updateUsername}
-                            value={username}></input>
-                    </div>
-                    <div>
-                        <label>Email</label>
-                        <input
-                            type="text"
-                            name="email"
-                            onChange={updateEmail}
-                            value={email}></input>
-                    </div>
-                    <div>
-                        <label>Password</label>
-                        <input
-                            type="password"
-                            name="password"
-                            onChange={updatePassword}
-                            value={password}></input>
-                    </div>
-                    <div>
-                        <label>Repeat Password</label>
-                        <input
-                            type="password"
-                            name="repeat_password"
-                            onChange={updateRepeatPassword}
-                            value={repeatPassword}
-                            required={true}></input>
-                    </div>
-                    <div className={styles.buttonContainer}>
-                        <button className={styles.button} type="submit">
-                            Sign Up
-                        </button>
+                        <div className={styles.emailWrapper}>
+                            <label className={styles.emailLabel}>Email:</label>
+                            <input
+                                type="text"
+                                name="email"
+                                onChange={updateEmail}
+                                value={email}></input>
+                        </div>
+                        <div className={styles.passwordWrapper}>
+                            <label className={styles.passwordLabel}>
+                                Password:
+                            </label>
+                            <input
+                                type="password"
+                                name="password"
+                                onChange={updatePassword}
+                                value={password}></input>
+                        </div>
+                        <div className={styles.passwordWrapper}>
+                            <label className={styles.passwordLabel}>
+                                Repeat Password:
+                            </label>
+                            <input
+                                type="password"
+                                name="repeat_password"
+                                onChange={updateRepeatPassword}
+                                value={repeatPassword}
+                                required={true}></input>
+                        </div>
+                        <div className={styles.buttonContainer}>
+                            <button className={styles.button} type="submit">
+                                Sign Up
+                            </button>
+                        </div>
                     </div>
                 </fieldset>
             </form>
