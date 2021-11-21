50a6f6386411df7ad550a3f109ccd30ec5992f8d
Robert Kauth <kauthr@gmail.com>
2021-11-21 11:07:53 -0800

    created join room and leave room event handlers

--- app/socket/socketio.py
+++ app/socket/socketio.py
@@ -1,5 +1,5 @@
 import os
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, send, join_room, leave_room
 
 
 # Configure CORS_origins based on environment
@@ -15,4 +15,26 @@ socketio = SocketIO(cors_allowed_origins=origins)
 
 @socketio.on('chat')
 def handle_chat(data):
+    '''
+    Chat event handler
+    '''
     emit("chat", data, broadcast=True)
+
+
+@socketio.on('join')
+def on_join(data):
+    '''
+    Handles user joining chat room
+    '''
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    send(username + ' has entered the room', t0=room)
+
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send(username + ' has left the room.', to=room)
