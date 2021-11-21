66b9d002e7a576658143d817f4307403512e7347
Robert Kauth <kauthr@gmail.com>
2021-11-20 16:35:49 -0800

    conditionally render nav buttons

--- react-app/src/components/Nav/NavBar.js
+++ react-app/src/components/Nav/NavBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import LogoutButton from "../Logout";
 import LoginModal from "../Login";
@@ -8,38 +9,47 @@ import styles from "./NavBar.module.css";
 // className={styles. }
 
 export default function NavBar() {
+    const sessionUser = useSelector((state) => state.session.user);
+
     return (
         <nav className={styles.nav}>
-            <div className={styles.navLeft}>
-                <div className={styles.home}>
-                    <NavLink
-                        to="/"
-                        exact={true}
-                        className={styles.link}
-                        activeClassName={styles.active}>
-                        Home
-                    </NavLink>
-                </div>
-                <div className={styles.users}>
-                    <NavLink
-                        to="/users"
-                        exact={true}
-                        className={styles.link}
-                        activeClassName={styles.active}>
-                        Users
-                    </NavLink>
-                </div>
-                <div className={styles.edit}>
-                    <ServerEditModal />
-                </div>
+            <div>
+                {sessionUser && (
+                    <div className={styles.navLeft}>
+                        <div className={styles.home}>
+                            <NavLink
+                                to="/"
+                                exact={true}
+                                className={styles.link}
+                                activeClassName={styles.active}>
+                                Home
+                            </NavLink>
+                        </div>
+                        <div className={styles.users}>
+                            <NavLink
+                                to="/users"
+                                exact={true}
+                                className={styles.link}
+                                activeClassName={styles.active}>
+                                Users
+                            </NavLink>
+                        </div>
+                        <div className={styles.edit}>
+                            <ServerEditModal />
+                        </div>
+                    </div>
+                )}
             </div>
             <div className={styles.navRight}>
-                <div className={styles.login}>
-                    <LoginModal />
-                </div>
-                <div className={styles.logout}>
-                    <LogoutButton />
-                </div>
+                {!sessionUser ? (
+                    <div className={styles.login}>
+                        <LoginModal />
+                    </div>
+                ) : (
+                    <div className={styles.logout}>
+                        <LogoutButton />
+                    </div>
+                )}
             </div>
         </nav>
     );
