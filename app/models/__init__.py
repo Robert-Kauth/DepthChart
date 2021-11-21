eb0d02e6e5c6973dc396a7c5ecac45f6b7b0351b
Robert Kauth <kauthr@gmail.com>
2021-11-21 14:08:09 -0800

    added user_message and message to models module

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,3 +3,5 @@ from .user import User
 from .server import Server
 from .user_server import User_server
 from .channel import Channel
+from .message import Message
+from .user_message import User_message
