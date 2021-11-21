2f1bd3221a884dd7090520e0872d80fb2f164e8e
Robert Kauth <kauthr@gmail.com>
2021-11-21 10:55:28 -0800

    created chat component

---
+++ react-app/src/components/Chat/Chat.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { io } from "socket.io-client";
+
+import styles from "./Chat.module.css";
+// className={styles. }
+
+let socket;
+export default function Chat() {
+    const user = useSelector((state) => state.session.user);
+
+    const [messages, setMessages] = useState([]);
+    const [chatInput, setChatInput] = useState("");
+
+    useEffect(() => {
+        socket = io();
+
+        socket.on("chat", (chat) => {
+            setMessages((messages) => [...messages, chat]);
+        });
+
+        return () => {
+            socket.disconnect();
+        };
+    }, []);
+
+    const updateChatInput = (e) => {
+        setChatInput(e.target.value);
+    };
+
+    const sendChat = (e) => {
+        e.preventDefault();
+
+        socket.emit("chat", { user: user.email, msg: chatInput });
+        setChatInput("");
+    };
+
+    return (
+        <div>
+            <div>
+                {messages.map((message, ind) => (
+                    <div key={ind}>{`${message.user}: ${message.msg}`}</div>
+                ))}
+            </div>
+            <form onSubmit={sendChat}>
+                <input value={chatInput} onChange={updateChatInput} />
+                <button type="submit">Send</button>
+            </form>
+        </div>
+    );
+}
