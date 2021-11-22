31bc8f6ab669103da256524149bb4033229b89a7
Robert Kauth <kauthr@gmail.com>
2021-11-21 16:27:33 -0800

    working on making message form recipients a dynamic select field

--- app/forms/message_form.py
+++ app/forms/message_form.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SelectField
+from wtforms.validators import DataRequired, InputRequired
+from app.models import User
 
 
 class MessageForm(FlaskForm):
-    recipients = StringField('recipients', validators=[DataRequired()])
-    message = StringField('message')
+    recipient_ids = SelectField('recipient_ids', coerce=int,
+                                validators=[InputRequired()])
+    message = StringField('message', validators=[DataRequired()])
