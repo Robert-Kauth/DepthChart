46c5fbce359f83f5d0a99bd6b4f4697cd4ce1b36
Robert Kauth <kauthr@gmail.com>
2021-11-28 11:42:48 -0800

    changed protected route so not redirected to login on refresh

--- react-app/src/components/auth/ProtectedRoute.js
+++ react-app/src/components/auth/ProtectedRoute.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { Route, Redirect } from 'react-router-dom';
+import React from "react";
+import { useSelector } from "react-redux";
+import { Route, Redirect } from "react-router-dom";
 
-const ProtectedRoute = props => {
-  const user = useSelector(state => state.session.user)
-  return (
-    <Route {...props}>
-      {(user)? props.children  : <Redirect to='/login' />}
-    </Route>
-  )
+const ProtectedRoute = (props) => {
+    const isOnline = useSelector((state) => state.session.online);
+    const user = useSelector((state) => state.session.user);
+    return (
+        <Route {...props}>
+            {user && isOnline ? (
+                props.children
+            ) : isOnline && !user ? (
+                <Redirect to="/login" />
+            ) : null}
+        </Route>
+    );
 };
-
 
 export default ProtectedRoute;
