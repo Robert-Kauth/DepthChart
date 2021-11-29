0adc945dac859f37cc5a3eb124058e2ceb8decb5
Robert Kauth <kauthr@gmail.com>
2021-11-28 16:35:20 -0800

    made message feed and message feed card components

--- react-app/src/components/Messages/MessageFeed.js
+++ react-app/src/components/Messages/MessageFeed.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { loadUsers } from "../../store/users";
+import { loadAllUserMessages } from "../../store/messages";
+import MessageFeedCard from "./MessageFeedCard";
 
 import styles from "./MessageFeed.module.css";
 // className={styles. }
 
 export default function MessageFeed() {
+    const dispatch = useDispatch();
+
+    const user = useSelector((state) => state.session.user);
+    const messages = useSelector((state) => state.messages.all_messages);
+
+    useEffect(() => {
+        dispatch(loadUsers());
+        dispatch(loadAllUserMessages(user.id));
+    }, [dispatch, user.id]);
     return (
         <>
-            <p className={styles.title}>Hello from message feed</p>
+            <p className={styles.title}>All Messages</p>
+            {messages &&
+                Object.values(messages).map((message) => (
+                    <MessageFeedCard key={message.id} message={message} />
+                ))}
         </>
     );
 }
