640f46b5c024b38e8fd4d9b3295541010ce0ad63
Robert Kauth <kauthr@gmail.com>
2021-11-30 10:26:27 -0800

    added users to sidebar

--- react-app/src/components/Friends/FriendCard.js
+++ react-app/src/components/Friends/FriendCard.js
@@ -1,10 +1,18 @@
 import React from "react";
-
-export default function FriendCard() {
+import styles from "./FriendCard.module.css";
+// className={styles. }
+export default function FriendCard({ users }) {
     return (
         <div>
-            <p>Friends name</p>
-            <div>Friends status</div>
+            {users &&
+                Object.values(users).map((user) => (
+                    <div className={styles.friendWrapper} key={user.id}>
+                        <p className={styles.name}>{user.username}</p>
+                        <div className={styles.imgContainer}>
+                            <img src={user.avatar} alt="user avatar" />
+                        </div>
+                    </div>
+                ))}
         </div>
     );
 }
--- react-app/src/components/Friends/Friends.js
+++ react-app/src/components/Friends/Friends.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 import FriendCard from "./FriendCard";
-
+import { loadUsers } from "../../store/users";
 import styles from "./Friends.module.css";
 // className={styles. }
 
 export default function Friends() {
+    const dispatch = useDispatch();
+
+    const users = useSelector((state) => state.users);
+
+    useEffect(() => {
+        dispatch(loadUsers());
+    }, [dispatch]);
+
     return (
         <div className={styles.friendsWrapper}>
             <div className={styles.titleWrapper}>
-                <p className={styles.title}>Friends</p>
+                <p className={styles.title}>Users</p>
             </div>
             <div>
-                <FriendCard />
+                <FriendCard users={users} />
             </div>
         </div>
     );
