353ce6267e79ce8133af032f90ba0411e72f203f
Robert Kauth <kauthr@gmail.com>
2021-12-26 17:32:27 -0800

    unnested navbar components

--- react-app/src/components/Nav/NavBar.js
+++ react-app/src/components/Nav/NavBar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
+
+import Avatar from "../User/Avatar";
 import LogoutButton from "../Logout";
 import LoginModal from "../Login";
 import ServerEditModal from "../Server/ServerEditModal";
@@ -9,39 +11,31 @@ import styles from "./NavBar.module.css";
 // className={styles. }
 
 export default function NavBar() {
-    const sessionUser = useSelector((state) => state.session.user);
+    const user = useSelector((state) => state.session.user);
 
     return (
         <nav className={styles.nav}>
-            <div>
-                {sessionUser && (
-                    <div className={styles.navLeft}>
-                        <div className={styles.home}>
-                            <NavLink
-                                to="/"
-                                exact={true}
-                                className={styles.link}
-                                activeClassName={styles.active}>
-                                Home
-                            </NavLink>
-                        </div>
-                        <div className={styles.edit}>
-                            <ServerEditModal />
-                        </div>
+            {user && (
+                <div className={styles.navLeft}>
+                    <div className={styles.home}>
+                        <NavLink
+                            to="/"
+                            exact={true}
+                            className={styles.link}
+                            activeClassName={styles.active}>
+                            Home
+                        </NavLink>
                     </div>
-                )}
-            </div>
+                    <div className={styles.edit}>
+                        <ServerEditModal />
+                    </div>
+                </div>
+            )}
             <div className={styles.navRight}>
                 <div className={styles.profileAvatar}>
-                    {sessionUser && (
-                        <img
-                            className={styles.img}
-                            src={sessionUser.avatar}
-                            alt="userAvatar"
-                        />
-                    )}
+                    {user && <Avatar user={user} />}
                 </div>
-                {!sessionUser ? (
+                {!user ? (
                     <div className={styles.login}>
                         <LoginModal />
                     </div>
