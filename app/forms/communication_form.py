e5f85dfccfc993ed311937586d19ce9fd3d0cff2
Robert Kauth <kauthr@gmail.com>
2022-01-03 09:45:49 -1000

    removed comment

--- app/forms/communication_form.py
+++ app/forms/communication_form.py
@@ -2,9 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField
 from wtforms.validators import DataRequired, InputRequired
 
-#! can be use for both messages and chats
-#! consider renaming to be something more general when you have the time
-
 
 class CommunicationForm(FlaskForm):
     recipients = SelectField('recipients', coerce=int,
