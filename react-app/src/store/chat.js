49724cc180636175017d52b7f3fd03e22aa00a5c
Robert Kauth <kauthr@gmail.com>
2022-01-03 08:41:43 -1000

    working on chat store

--- react-app/src/store/chat.js
+++ react-app/src/store/chat.js
@@ -5,8 +5,38 @@ const CREATE = "chat/CREATE";
 const EDIT = "chat/EDIT";
 const DESTROY = "chat/DESTROY";
 /*-------------ACTIONS-------------*/
-export const loadChats = () => async (dispatch) => {
-    const res = fetch("/api/chats/");
-};
+
+const loadAll = (chats) => ({
+    type: LOAD_ALL,
+    chats,
+});
+
+const loadOne = (chat) => ({
+    type: LOAD_ONE,
+    chat,
+});
+
+const create = (chat) => ({
+    type: CREATE,
+    chat,
+});
+
+const edit = (chat) => ({
+    type: EDIT,
+    chat,
+});
+
+const destroy = (id) => ({
+    type: DESTROY,
+    id,
+});
+
 /*-------------THUNK CREATORS-------------*/
+export const loadChat = (chat_id) => async (dispatch) => {
+    const res = fetch(`/api/chats/${chat_id}`);
+    if (res.ok) {
+        const chat = res.json();
+        dispatch();
+    }
+};
 /*-------------REDUCER-------------*/
