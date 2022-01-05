052e05cef35a692fcdedbd13a61613a87649b4f6
Robert Kauth <kauthr@gmail.com>
2022-01-05 07:52:11 -1000

    separated friend components

--- react-app/src/components/Friends/Friends.js
+++ react-app/src/components/Friends/Friends.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
-import FriendCard from "./FriendCard";
+import FriendCard from "../FriendCard";
 import { loadUsers } from "../../store/users";
 import styles from "./Friends.module.css";
 // className={styles. }
