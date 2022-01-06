31db471ca01c64ce69b74a51d69d2cb597314de9
Robert Kauth <kauthr@gmail.com>
2022-01-06 07:51:22 -1000

    fixed default number of users per server generated

--- app/seeds/user_servers.py
+++ app/seeds/user_servers.py
@@ -6,11 +6,12 @@ faker = Faker()
 # total numbers
 total_users = 25
 total_servers = 50
+total_users_servers = 505
+total_users_per_server = 10
 
 # generated numbers
 generated_users = 21
 generated_servers = 45
-generated_user_servers = 5
 generated_server_channels = 5
 generated_messages_per_channel = 5
 
