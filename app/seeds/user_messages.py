c40fa9592525c6c8036799ec5475e727090a39b4
Robert Kauth <kauthr@gmail.com>
2022-01-06 08:20:57 -1000

    added defaults file and working on user_messages seeder file

--- app/seeds/user_messages.py
+++ app/seeds/user_messages.py
@@ -1,13 +1,24 @@
 from app.models import db, User_message
+from faker import Faker
+
+faker = Faker()
 
 # total numbers
 total_users = 25
 total_servers = 50
 
+total_users_servers = 505
+total_users_per_server = 10
+
+total_channels = 250
+total_channels_per_server = 5
+
+total_messages = 200
+
 # generated numbers
 generated_users = 21
 generated_servers = 45
-generated_user_servers = 5
+generated_user_servers = 500
 generated_server_channels = 5
 generated_messages_per_channel = 5
 
@@ -16,6 +27,8 @@ def seed_user_messages():
     '''
     Seeds user_messages
     '''
+    for i in range(1, 200):
+        pass
     demo_user_message0 = User_message(sender_id=1,
                                       recipient_ids=2, message_id=1, is_read=False)
     demo_user_message1 = User_message(sender_id=1,
