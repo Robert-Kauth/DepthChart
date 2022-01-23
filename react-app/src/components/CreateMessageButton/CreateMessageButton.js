cecdd6800bf6c0488e5915f6d79450b7cd92b3c9
Robert Kauth <kauthr@gmail.com>
2022-01-23 11:06:26 -0800

    removed unused css module for createmessage button

--- react-app/src/components/CreateMessageButton/CreateMessageButton.js
+++ react-app/src/components/CreateMessageButton/CreateMessageButton.js
@@ -3,9 +3,6 @@ import { mdiPlusBox } from "@mdi/js";
 import StyledButton from "../StyledButton";
 import NewMessageForm from "../NewMessageForm";
 
-import styles from "./CreateMessageBar.module.css";
-// className={styles. }
-
 export default function CreateMessageButton() {
     return <StyledButton icon={mdiPlusBox} form={NewMessageForm} />;
 }
