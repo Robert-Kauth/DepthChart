09d0bff7c4eff9be8779187cc86e85827e53b428
Robert Kauth <kauthr@gmail.com>
2022-01-24 08:46:45 -0800

    removed unused import from friendcard component

--- react-app/src/components/FriendCard/FriendCard.js
+++ react-app/src/components/FriendCard/FriendCard.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
 
 import ChatButton from "../ChatButton/ChatButton";
 import UserInfo from "../UserInfo";
