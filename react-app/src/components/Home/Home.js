60dc1d2ee8dd62524102a5f35cfcbb361fec1772
Robert Kauth <kauthr@gmail.com>
2022-01-26 15:48:35 -0800

    debugged duplicates in messages

--- react-app/src/components/Home/Home.js
+++ react-app/src/components/Home/Home.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
 
 import Servers from "../Servers";
 import Main from "../Main";
@@ -7,27 +7,11 @@ import Messages from "../Messages";
 import MessageFeed from "../MessageFeed";
 import Friends from "../Friends";
 
-import { loadMessagesBetween } from "../../store/messages";
-
 import styles from "./Home.module.css";
 // className={styles. }
 
 export default function Home() {
-    const dispatch = useDispatch();
-
     const messages = useSelector((state) => state.messages.between);
-    const selectedMsg = useSelector((state) => state.messages.message);
-
-    let user1_id;
-    let user2_id;
-    if (selectedMsg) {
-        user1_id = selectedMsg.recipient_id;
-        user2_id = selectedMsg.sender_id;
-    }
-
-    useEffect(() => {
-        dispatch(loadMessagesBetween(user1_id, user2_id));
-    }, [dispatch, user1_id, user2_id]);
 
     return (
         <div className={styles.homeBackground}>
@@ -36,16 +20,10 @@ export default function Home() {
                     <Servers />
                 </div>
                 <div className={styles.main}>
-                    {selectedMsg && messages ? (
+                    {messages ? (
                         <Main
                             card={<Messages />}
-                            feed={
-                                <MessageFeed
-                                    messages={messages}
-                                    recipient_id={user1_id}
-                                    sender_id={user2_id}
-                                />
-                            }
+                            feed={<MessageFeed messages={messages} />}
                         />
                     ) : (
                         <Main card={<Messages />} />
--- react-app/src/components/MessageFeed/MessageFeed.js
+++ react-app/src/components/MessageFeed/MessageFeed.js
@@ -1,45 +1,26 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
 
 import MessageFeedCard from "../MessageFeedCard";
 import Title from "../Title";
 import CreateMessageBar from "../CreateMessageBar";
 import FeedTitleBar from "../FeedTitleBar";
 
-import { loadAllUserMessages } from "../../store/messages";
-
 import styles from "./MessageFeed.module.css";
 // className={styles. }
 
-export default function MessageFeed({ messages, sender_id, recipient_id }) {
-    const dispatch = useDispatch();
-    const user = useSelector((state) => state.session.user);
-    const users = useSelector((state) => state.users.all);
-
-    useEffect(() => {
-        dispatch(loadAllUserMessages(user.id));
-    }, [dispatch, user.id]);
-
-    // Determine other messaged user
-    let other_user_id;
-    let other_user;
-    if (recipient_id === user.id) {
-        other_user_id = sender_id;
-        other_user = users[other_user_id];
-    } else {
-        other_user_id = recipient_id;
-        other_user = users[other_user_id];
-    }
+export default function MessageFeed({ messages }) {
+    const user2_id = useSelector((state) => state.users.user);
 
     return (
         <div className={styles.wrapper}>
             <Title title="User Messages" />
-            <FeedTitleBar user={other_user} />
+            <FeedTitleBar user={user2_id} />
             {messages &&
                 Object.values(messages).map((message) => (
                     <MessageFeedCard key={message.id} message={message} />
                 ))}
-            <CreateMessageBar recipient_id={other_user_id} />
+            <CreateMessageBar recipient_id={user2_id.id} />
         </div>
     );
 }
